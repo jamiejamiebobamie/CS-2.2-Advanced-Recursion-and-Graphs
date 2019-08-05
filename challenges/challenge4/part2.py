b57150b53d490744808440279e17fb0aad44622f
jamiejamiebobamie <jmccrory@vt.edu>
2019-08-05 12:34:06 -0700

    added memoization to to part2.py

--- challenges/challenge4/part2.py
+++ challenges/challenge4/part2.py
@@ -10,67 +10,61 @@
 
 
 
-"""Example:
-    Input:
-        length = [1,2,3,4,5,6,7,8]
-        price = [1,5,8,9,10,17,17,20]
-        rod_length = 4
 
-    Best: Cut the rod into two pieces of length 2 each to gain revenue of 5+5 = 10
+def driver_function(prices,n):
+    dict = {}
+    def rodCut(prices,n):
+        """
+            Note:
+                this function was found in C++ on: https://www.techiedelight.com/rot-cutting/
+            Input:
+                an array of prices with indexes that correlate to the size of the pieces of wood.
+                an integer of the length of the wood to be cut.
+            Output:
+                a string of the inputs and the resulting max cost that can be made from the length of the wood and the prices.
+        """
 
-    Cut     Profit
-    4       9
-    1,3     (1+8) = 9
-    2,2     (5+5) = 10
-    3,1     (8+1) = 9
-    1,1,2   (1+1+5) = 7
-    1,2,1   (1+5+1) = 7
-    2,1,1   (5+1+1) = 7
-    1,1,1,1 (1+1+1+1) = 4"""
+        # each time the function gets called initialize maxValue to be negative infinity
+        maxValue = float('-inf')
 
-def findBestCut(length, price, rod_length, cut):
-    # my attempt:
-    if cut == 0:
-        return
-    if rod_length not in length:
-        return
-    else:
-        findBestCut(length, price, rod_length, cut-1)
+        # if the input length of the rod is 0, return 0
+        if n == 0:
+            return 0
 
-# C++ solution found on:
-# https://www.techiedelight.com/rot-cutting/
-def rodCut(prices,n, dict):
-    """This is super cool, but I don't really understand this at all.
-    """
+        # generate numbers between 1 and the current rod_length + 1 (+1 because range() is non-inclusive at the upper bounds)
+        for _ in range(1, n+1):
 
-    # if not maxValue:
-    maxValue = float('-inf')
+            # set 'entry' to a tuple of the current cut. a cut consists of two pieces.
+            # cut == piece A and piece B: A is: _ and piece B is: the length of the rod - piece A.
+            cut = (_, n-_)
 
-    if n == 0:
-        return 0
+            # memoization dictionary:
+            if cut in dict:
+                cost = dict[cut]
+            else:
+            # reference the price for piece A, taking into account the index of piece A will be: _-1
+            # need to determine the cost(s) of all pieces resulting from that cut.
+            # so piece B is fed into "the wood chipper": the rodCut function, to determine its cost(s)
+                cost = prices[_-1] + rodCut(prices, n - _)
+                dict[cut] = cost
 
-    for _ in range(1, n+1):
-        entry = (_-1, n-_)
+            # if the resuting cost is greater than the local maxValue set the local maxValue to the cost
+            if (cost > maxValue):
+                maxValue = cost
 
-        # memoization:
-        if entry not in dict:
-            cost = prices[_-1] + rodCut(prices, n - _, dict)
-            dict[entry] = cost
-        else:
-            cost = dict[entry]
+        # return the maxValue to the outside scope.
+        return maxValue
 
-        if (cost > maxValue):
-            maxValue = cost
+    maxValue = rodCut(prices,n)
+    printable_prices = [str(price) for price in prices]
 
-    return maxValue
+    return "For this input:\n\n"+ "prices: " + ", ".join(printable_prices) + "\nwood length: " + str(n) + "\n\nThe solution is:\n\n" + str(maxValue)
 
-length = [1,2,3,4,5,6,7,8]
-rod_length = 4
 
-prices = [1,5,8,9,10,17,17,20]
-# prices = [1,5,8,9,10,17,17,15]
+# i + 1 == wood length: [1,2,3,4,5,6,7,8,9,10]
+prices = [1,5,8,9,10,17,17,20,24,26]
 
-n = 8
+# the length of the piece of wood
+n = 10
 
-dict = {}
-print(rodCut(prices, n, dict))
+print(driver_function(prices, n))
